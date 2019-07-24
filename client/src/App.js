40408c35c9c5482ef4817c469b455609ca809e04
alexdo21 <alex.do21@yahoo.com>
2019-07-24 12:15:03 -0500

    search component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'; 
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom"
 
-import './App.css';
-import Front from './pages/Front';
+import './App.css'
+import Front from './pages/Front'
 import Login from './components/Login'
 import Register from './components/Register'
+import Search from './components/Search'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path='/' component={Front}/>
         <Route path='/login' component={Login}/>
         <Route path='/register' component={Register}/>
+        <Route path='/search' component={Search}/>
       </Switch>
     </Router>
   );
