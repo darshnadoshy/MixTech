d4313acbeb65d3b39d3f62cf534813bab094362a
Taijing Chen <taijingchen99@gmail.com>
2019-07-24 11:08:28 -0500

    JwtUtils

--- server/src/main/java/com/example/cs564/controller/LoginController.java
+++ server/src/main/java/com/example/cs564/controller/LoginController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
-public class UserController {
+@RequestMapping(value = "/login")
+public class LoginController {
 }
