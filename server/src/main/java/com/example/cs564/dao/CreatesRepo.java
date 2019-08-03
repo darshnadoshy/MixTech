607a96b9843aa0c5adb5b3143555ea0e4f919946
nkimball1 <nkimball@wisc.edu>
2019-08-03 12:57:21 -0500

    Match work- Routes for create, delete fully functional on match/creates relational tables.

--- server/src/main/java/com/example/cs564/dao/CreatesRepo.java
+++ server/src/main/java/com/example/cs564/dao/CreatesRepo.java
@@ -1,11 +1,11 @@
 package com.example.cs564.dao;
 
-import com.example.cs564.entity.MatchEntity;
+import com.example.cs564.entity.CreatesEntity;
 import com.example.cs564.entity.key.CreatesKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MatchRepo extends JpaRepository<MatchEntity, Long> {
+public interface CreatesRepo extends  JpaRepository<CreatesEntity, CreatesKey> {
 
 }
--- server/src/main/java/com/example/cs564/entity/key/CreatesKey.java
+++ server/src/main/java/com/example/cs564/entity/key/CreatesKey.java
@@ -4,5 +4,14 @@ import java.io.Serializable;
 
 public class CreatesKey implements Serializable {
     private Long uid;
-    private Long pid;
+    private Long mid;
+
+    public CreatesKey() {
+
+    }
+
+    public CreatesKey(long uid, Long mid) {
+        this.uid = uid;
+        this.mid =mid;
+    }
 }
