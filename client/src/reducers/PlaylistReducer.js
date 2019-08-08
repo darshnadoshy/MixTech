0dc17f0a6178d36b7d752ccbd2ee48ca2b1af8a6
Taijing Chen <taijingchen99@gmail.com>
2019-08-08 18:09:12 -0500

    advance search

---
+++ client/src/reducers/PlaylistReducer.js
@@ -0,0 +1,23 @@
+
+
+const initialState = {
+    results: []
+};
+
+
+export default (state=initialState, action) => {
+    switch (action.type) {
+        case 'ALL_PLAYLISTS':
+            return {
+                state,
+                results: action.payload
+            };
+        case 'CLEAR':
+            return {
+                state,
+                results: []
+            };
+        default:
+            return state
+    }
+}
