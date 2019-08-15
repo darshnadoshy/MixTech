1034584d0e95d5a2bed86b0abd1189efabfd9e79
nkimball1 <nkimball@wisc.edu>
2019-08-14 20:24:37 -0500

    More Comments

--- server/src/main/java/com/example/cs564/controller/MatchController.java
+++ server/src/main/java/com/example/cs564/controller/MatchController.java
@@ -17,6 +17,11 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class handles all requests related to matches. Each method takes some
+ * form of input and passes it to a corresponding service class which
+ * performs the necessary actions.
+ */
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/match")
@@ -49,18 +54,33 @@ public class MatchController {
         return songs;
     }
 
+    /** Get all the matches from a user
+     *
+     * @param uid ID of the user to retrieve all matches from
+     * @return List of match responses
+     */
     @ResponseBody
     @RequestMapping(value = "/all/{uid}", method = RequestMethod.GET)
     public List<DisplayMatchResponse> displayMatchByUid(@PathVariable Long uid) {
         return matchService.displayMatchByUid(uid);
     }
 
+    /** Get all the incomplete matches from a user
+     *
+     * @param uid ID of the user to retrieve all incomplete matches from
+     * @return List of only incomplete match responses
+     */
     @ResponseBody
     @RequestMapping(value = "/incomplete/{uid}", method = RequestMethod.GET)
     public List<DisplayMatchResponse> displayIncompleteMatchByUid(@PathVariable Long uid) {
         return matchService.displayIncompleteMatchByUid(uid);
     }
 
+    /** Get all of the COMPLETE matches from a user
+     *
+     * @param uid ID of the user to retrieve from
+     * @return List of only complete matches belonging to a user
+     */
     @ResponseBody
     @RequestMapping(value = "/complete/{uid}", method = RequestMethod.GET)
     public List<DisplayMatchResponse> displayCompleteMatchByUid(@PathVariable Long uid) {
@@ -77,13 +97,24 @@ public class MatchController {
 //        createsService.create(createsEntity);
 //    }
 
+    /** Create a new match and optionally set the first song
+     *
+     * @param spotifyUri1 URI of the FIRST song in a match
+     * @param uid User that the match will belong to.
+     */
     @ResponseBody
     @RequestMapping(value = "/create/{uid}", method = RequestMethod.POST)
     public void create(@RequestParam String spotifyUri1, @PathVariable Long uid) {
         matchService.create(spotifyUri1, uid);
     }
 
-
+    /** Add a song to an incomplete match
+     *
+     * @param uid The user ID of the match creator
+     * @param mid The Match ID to add the song too
+     * @param spotifyUri2 The spotify ID of the SECOND song in the matches
+     * @return The response to the request stating success or failure
+     */
     @ResponseBody
     @RequestMapping(value = "/addsong/{uid}/{mid}", method = RequestMethod.POST)
     public StandardResponse addSong(@PathVariable Long uid, @PathVariable Long mid, @RequestParam String spotifyUri2) {
@@ -105,7 +136,11 @@ public class MatchController {
         return response;
     }
 
-    //Delete a match by ID
+    /** Remove a match from the matches and creates tables.
+     *
+     * @param mid ID of the match to delete
+     * @param uid ID of match creator
+     */
     @RequestMapping(value = "/delete/{mid}/{uid}", method = RequestMethod.DELETE)
     public void remove(@PathVariable Long mid, @PathVariable long uid) {
         CreatesKey key = new CreatesKey(uid, mid);
